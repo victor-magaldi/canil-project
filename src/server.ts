0bbd55c89b02cc479893c847858f9305186a9e00
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-12 18:49:08 -0300

    first render ejs

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,20 @@ import dotenv from "dotenv";
 import ejs from "ejs";
 import path from "path"
 
+dotenv.config()
 
+
+const app = express()
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/", function (req, res) {
+  res.render("teste");
+});
+
+app.listen(process.env.PORT);
 console.log("teste")
