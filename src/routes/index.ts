175174ed904f03f33454bc971f11a32e6bedb84a
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-13 19:33:27 -0300

    create routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import {Router} from "express"
+import * as PageController from "../controllers/pageController"
+import * as SearchController from "../controllers/searchController";
 
 const router = Router()
 
-router.get("/teste", function (req, res) {
-  res.render("teste", { name: "Victor de Souza Magaldi" });
-});
+router.get("/", PageController.home);
+router.get("/dogs", PageController.dogs);
+router.get("/cats", PageController.cats);
+router.get("/fishes", PageController.fishes);
+router.get("/search", SearchController.search);
+
+
 
 export {router}
