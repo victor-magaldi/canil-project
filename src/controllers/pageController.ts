26ced64cd333825877377f92a175029d0ecc1e74
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-18 18:42:34 -0300

    rename page

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,7 @@
 import { Request,Response } from "express";
 
 export const home = (req:Request, res:Response)=>{
-    res.render("pages/home");
+    res.render("pages/page");
 }
 
 export const dogs = (req: Request, res: Response) => {};
