f5967ded36f1d68ccaf344e2b0c1dbd37ddc6f52
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-18 18:49:12 -0300

    first title dynamic EJS

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,12 @@
 import { Request,Response } from "express";
 
 export const home = (req:Request, res:Response)=>{
-    res.render("pages/page");
+    res.render("pages/page", {
+      banner: {
+        title: "Todos os animais disponíveis para adoção",
+        backgroundUrl: "/images/allanimals.jpg",
+      },
+    });
 }
 
 export const dogs = (req: Request, res: Response) => {};
