cf90bbeaac994b23e9449e4092241c0b2c4c1006
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-19 18:54:52 -0300

    page controller add menu content

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,39 +1,63 @@
 import { Request, Response } from 'express'
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page', {
-        titlePage: 'Todos os animais disponíveis para adoção',
-        banner: {
-            title: 'Todos os animais',
-            backgroundUrl: '/images/allanimals.jpg',
-        },
-    })
+  res.render('pages/page', {
+    menu: {
+      all: true,
+      dogs: false,
+      cats: false,
+      fishes: false,
+    },
+    titlePage: 'Todos os animais disponíveis para adoção',
+    banner: {
+      title: 'Todos os animais',
+      backgroundUrl: '/images/allanimals.jpg',
+    },
+  })
 }
 
 export const dogs = (req: Request, res: Response) => {
-    res.render('pages/page', {
-        titlePage: 'Cachorros disponíveis para adoção',
-        banner: {
-            title: 'Cachorros',
-            backgroundUrl: '/images/banner_dog.jpg',
-        },
-    })
+  res.render('pages/page', {
+    menu: {
+      all: false,
+      dogs: true,
+      cats: false,
+      fishes: false,
+    },
+    titlePage: 'Cachorros disponíveis para adoção',
+    banner: {
+      title: 'Cachorros',
+      backgroundUrl: '/images/banner_dog.jpg',
+    },
+  })
 }
 export const cats = (req: Request, res: Response) => {
-    res.render('pages/page', {
-        titlePage: 'Gatos disponíveis para adoção',
-        banner: {
-            title: 'Gatos',
-            backgroundUrl: '/images/banner_cat.jpg',
-        },
-    })
+  res.render('pages/page', {
+    menu: {
+      all: false,
+      dogs: false,
+      cats: true,
+      fishes: false,
+    },
+    titlePage: 'Gatos disponíveis para adoção',
+    banner: {
+      title: 'Gatos',
+      backgroundUrl: '/images/banner_cat.jpg',
+    },
+  })
 }
 export const fishes = (req: Request, res: Response) => {
-    res.render('pages/page', {
-        titlePage: 'peixes disponíveis para adoção',
-        banner: {
-            title: 'peixes',
-            backgroundUrl: '/images/banner_fish.jpg',
-        },
-    })
+  res.render('pages/page', {
+    menu: {
+      all: false,
+      dogs: false,
+      cats: false,
+      fishes: true,
+    },
+    titlePage: 'peixes disponíveis para adoção',
+    banner: {
+      title: 'peixes',
+      backgroundUrl: '/images/banner_fish.jpg',
+    },
+  })
 }
