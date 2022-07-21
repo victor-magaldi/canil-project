7ad8642a60cbcb7c2c23f39a1156a17c53f0b35c
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-21 19:11:44 -0300

    page validation list

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -4,10 +4,14 @@ import { Pet } from '../models/pet'
 import { createMenuObj } from '../helpers/createMenuObj'
 
 export const search = (req: Request, res: Response) => {
+  let query: string = req.query.q as string
+
+  let list = Pet.getFromName(query)
+
   res.render('pages/page', {
     menu: createMenuObj(''),
     banner: false,
-    titlePage: 'Página não econtrada',
-    list: [],
+    titlePage: 'Busca por:' + query,
+    list,
   })
 }
