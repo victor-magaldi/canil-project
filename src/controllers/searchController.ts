7e0f30f0484dcb1235328b45154e462fd1e5571f
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-21 18:46:28 -0300

    not found page

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,3 +1,13 @@
 import { Request, Response } from 'express'
 
-export const search = (req: Request, res: Response) => {}
+import { Pet } from '../models/pet'
+import { createMenuObj } from '../helpers/createMenuObj'
+
+export const search = (req: Request, res: Response) => {
+  res.render('pages/page', {
+    menu: createMenuObj(''),
+    banner: false,
+    titlePage: 'Página não econtrada',
+    list: [],
+  })
+}
